98120aaf93777cc15ed6164883aa60781679e205
vasanthg1210 <32510142+vasanthg1210@users.noreply.github.com>
2018-03-09 15:47:22 +0530

    Update task.sh

--- tasks/upload-product-and-stemcell/task.sh
+++ tasks/upload-product-and-stemcell/task.sh
@@ -79,8 +79,11 @@ if [ -n "$STEMCELL_VERSION" ]; then
 fi
 
 # Should the slug contain more than one product, pick only the first.
-FILE_PATH=`find ./pivnet-product -name *.pivotal | sort | head -1`
-om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+productCount=$(ls pivnet-product/*.pivotal 2> /dev/null | wc -l)
+if [[ $productCount != 0 ]]; then
+    for file in pivnet-product/*.pivotal; do
+        echo "============ Uploading product $file Begin ============"
+        om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
   --client-id "${OPSMAN_CLIENT_ID}" \
   --client-secret "${OPSMAN_CLIENT_SECRET}" \
   -u "$OPS_MGR_USR" \
@@ -89,3 +92,7 @@ om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
   --request-timeout 3600 \
   upload-product \
   -p $FILE_PATH
+        echo "============ Uploading product $file End ============"
+    done
+fi
+
