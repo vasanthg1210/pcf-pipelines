92afc9464bff8d0a702cd564b2fad9038997803e
vasanthg1210 <32510142+vasanthg1210@users.noreply.github.com>
2018-03-12 12:23:53 +0530

    Update task.sh

--- tasks/upload-product-and-stemcell/task.sh
+++ tasks/upload-product-and-stemcell/task.sh
@@ -86,8 +86,8 @@ if [[ $productCount != 0 ]]; then
         om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
   --client-id "${OPSMAN_CLIENT_ID}" \
   --client-secret "${OPSMAN_CLIENT_SECRET}" \
-  -u "$OPS_MGR_USR" \
-  -p "$OPS_MGR_PWD" \
+  -u "pcflab" \
+  -p "pcflab" \
   -k \
   --request-timeout 3600 \
   upload-product \
